0e04d5136a0253ec875dee25af155dae274788c2
zachary <zackzackzackw@gmail.com>
2014-07-31 16:49:36 -0400

    wrote a bunch of functions to run and validate a single network
    function to look at correlation of prior interactions in data
    function to look at weight of prior interactions in network
    benchmarking for b_subtilis network
    prediction performance good

--- bacteria.py
+++ bacteria.py
@@ -2,11 +2,11 @@ import numpy as np
 import fused_L2 as fl
 
 def run_scr(lamP, lamR, lamS, outf):
-    (ba,tf, genes, tfs) = load_bacteria('B_subtilis.csv','tfNames.txt')
-    BC_priors = load_priors('gsSDnamesWithActivitySign082213','B_subtilis')
+    (ba,tf, genes, tfs) = load_bacteria('B_subtilis.csv','tfNames.txt',range(250))
+    (BC_priors, sign) = load_priors('gsSDnamesWithActivitySign082213','B_subtilis')
     orth = load_orth('',['B_subtilis'])
 
-
+    print ba.shape
     organisms = ['B_subtilis']
     gene_ls = [genes]
     tf_ls = [tfs]
@@ -27,21 +27,71 @@ def run_scr(lamP, lamR, lamS, outf):
     f.write('\n'.join(Bs_str_l))
     f.close()
 
-def load_priors(priors_fn, organism):
-    
+def load_priors(priors_fn, organism):    
     p = file(priors_fn)
     ps = p.read()
     psn = filter(len, ps.split('\n'))
     psnt = map(lambda x: x.split('\t'), psn)
     priors = map(lambda x: (fl.one_gene(x[0], organism), fl.one_gene(x[1], organism)), psnt)
+    signs = map(lambda x: [-1,1][x[2]=='activation'], psnt)
     p.close()
-    return priors
+    return (priors,signs)
 
 #TODO pending seeing the orth file format
 def load_orth(orth_fns, organisms):
     return []
 
-def load_bacteria(expr_fn, tfs_fn):
+def load_network(net_fn):
+    f = file(net_fn)
+    fs = f.read()
+    fsl = filter(len, fs.split('\n'))
+    fslt = map(lambda x: x.split('\t'), fsl)
+    tfs = fslt[0]
+    genes = map(lambda x: x[0], fslt[1:])
+    #the network is written as the transpose of the matrix we want
+    net = np.zeros((len(genes), len(tfs)))
+    for g in range(len(genes)):
+        targets = np.array(map(float, fslt[g+1][1:]))
+        net[g, :] = targets
+    return (net.T, genes, tfs)
+
+
+def eval_prediction(net_fn, expr_fn, tfs_fn, metric, sub_conds=[]):
+    (net, genes, tfs) = load_network(net_fn)
+    (e, t, genes, tfs) = load_bacteria(expr_fn, tfs_fn, sub_conds)
+    
+    err = fl.prediction_error(t, net, e, metric)
+    return err
+#looks at a network and returns an array of interaction weights for all priors in the network, along with an array containing the sign of the prior (+1, -1)
+def check_prior_recovery(net_fn, priors_fn):
+    (net, genes, tfs) = load_network(net_fn)
+    (p, sign) = load_priors(priors_fn, '1')
+    pcon = fl.priors_to_constraints(['1'], [genes],[tfs],p,0.5)
+    prior_inter = []
+    for con in pcon:
+        prior_inter.append(net[con.c1.r, con.c1.c])
+    
+    return (np.array(prior_inter), np.array(sign))
+            
+#returns a list of tf gene correlations for each prior interaction, as well as the sign of each prior
+def check_prior_corr(expr_fn, tfs_fn, priors_fn):
+    (e, t, genes, tfs) = load_bacteria(expr_fn, tfs_fn)
+    (p, s) = load_priors(priors_fn,'1')
+    pcon = fl.priors_to_constraints(['1'], [genes],[tfs],p,0.5)
+
+    prior_corr = []
+    for con in pcon:
+        tfi = con.c1.r
+        gi = con.c1.c
+        tf_expr = t[:, tfi]
+        g_expr = e[:, gi]
+        corr  = np.corrcoef(tf_expr, g_expr)[0,1]
+        prior_corr.append(corr)
+    return (prior_corr, s)
+
+
+#genes are mean 0
+def load_bacteria(expr_fn, tfs_fn, sub_conds=[]):
     f = file(expr_fn)
     fs = f.read()
     fsl = filter(len, fs.split('\n'))
@@ -71,7 +121,12 @@ def load_bacteria(expr_fn, tfs_fn):
         gi = gene_to_ind[tfs[ti]]
         tf_mat_t[ti, :] = exp_mat_t[gi, :]
 
+    exp_mat = (exp_mat_t - np.mean(exp_mat_t, axis=0)).T
+    tf_mat = tf_mat_t.T
+    if sub_conds == []:
+        sub_conds = range(exp_mat.shape[0])
+        
+    sub_conds = np.array(sub_conds)
+    return (exp_mat[sub_conds, :], tf_mat[sub_conds, :], genes, tfs)
 
-    return (exp_mat_t.T, tf_mat_t.T, genes, tfs)
-
-run_scr(0.1,50,0,'wat2.tsv')
+f = lambda x: run_scr(0.001,5,0,'wat4.tsv')
